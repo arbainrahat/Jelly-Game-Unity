c084c368c8d6dc1d47eb9234330455708e8dc1d8
Arbain <arbainrahat@gmail.com>
2020-08-24 00:45:26 +0500

    commit

---
+++ Jelly Runner/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed;
+
+    public GameObject effect;
+    private Animator camAnim;
+
+    public AudioClip enemyExpoldeClip;
+
+    private void Start()
+    {
+        camAnim = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+        
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            AudioSource.PlayClipAtPoint(enemyExpoldeClip, transform.position);
+
+            
+            camAnim.SetTrigger("shake");
+            Instantiate(effect, transform.position, Quaternion.identity);
+            collision.GetComponent<Player>().health -= damage;
+            
+            // Debug.Log(collision.GetComponent<Player>().health);
+            
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Jelly Runner/Assets/Scripts/Player.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Player : MonoBehaviour
+{
+
+    private Vector2 targetPos;
+    public float Yincriment;
+
+    public float speed;
+    public float maxHieght;
+    public float minHieght;
+
+    public int health = 3;
+
+    public GameObject effect;
+    public Animator camAnim;
+
+    public Text healthDisplay;
+
+    public GameObject gameOver;
+
+    public AudioClip gameOverSound;
+
+    public GameObject camra;
+
+    void Update()
+    {
+        healthDisplay.text = health.ToString();
+
+        //Reload the Scene when player health zero or less
+
+        if(health <= 0)
+        {
+
+            // Disable the Spawner Component than Obstacles will not instiate
+
+            FindObjectOfType<Spawner>().GetComponent<Spawner>().enabled=false;
+
+           
+           
+
+            camra.GetComponent<AudioSource>().Stop();
+
+            AudioSource.PlayClipAtPoint(gameOverSound, transform.position);
+            gameOver.SetActive(true);
+
+            
+            
+
+            Destroy(gameObject);
+        }
+
+        //It smoothly move character
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed*Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow ) && transform.position.y < maxHieght)
+        {
+            camAnim.SetTrigger("shake");
+            Instantiate(effect,transform.position,Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincriment);
+            //transform.position = targetPos;             //It Snap from one position to other
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHieght)
+        {
+            camAnim.SetTrigger("shake");
+            Instantiate(effect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincriment);
+            //transform.position = targetPos;
+        }
+    }
+}
